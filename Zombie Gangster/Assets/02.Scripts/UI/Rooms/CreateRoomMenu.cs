f53c654eccccfa2c94ee0b41708a730fbfd768f7
shinjungsub <48083280+shinjungsub@users.noreply.github.com>
2019-05-25 04:32:47 +0900

    ## 신정섭 ## Photon을 사용하여 기본적인 UI 만드는 중임

---
+++ Zombie Gangster/Assets/02.Scripts/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks {
+
+    [SerializeField]
+    private Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+}
---
+++ Zombie Gangster/Assets/TestConnect.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks {
+
+	// Use this for initialization
+	void Start () {
+        print("Connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+	}
+
+    public override void OnConnectedToMaster()
+    {
+        //MasterManager.DebugConsole.AddText("Connected to Photon.", this);
+        Debug.Log("Connected to Photon", this);
+
+        Debug.Log("My nickname is " + PhotonNetwork.LocalPlayer.NickName, this);
+        if (!PhotonNetwork.InLobby)
+            PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Failed to connect to Photon: " + cause.ToString(), this);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Joined lobby");
+    }
+}

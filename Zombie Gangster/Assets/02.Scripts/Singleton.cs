264e47e25b77b936d5c8272fe988a770aa34e1a5
joseph0607 <shinjs94@gmail.com>
2019-05-28 14:41:55 +0900

    ## 신정섭 ## 포톤아 안녕

--- Zombie Gangster/Assets/02.Scripts/Singleton.cs
+++ Zombie Gangster/Assets/02.Scripts/Singleton.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-using Photon.Realtime;
 
 public class Singleton : MonoBehaviour {
     static Singleton current = null;

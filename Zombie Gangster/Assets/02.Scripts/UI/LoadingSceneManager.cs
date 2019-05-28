0e6d7ed987a1ed387d19cba6b45d9467a9d34444
lee394 <leeleelee394@gmail.com>
2019-05-28 14:17:22 +0900

    ##이주희## 로비수정

--- Zombie Gangster/Assets/02.Scripts/UI/LoadingSceneManager.cs
+++ Zombie Gangster/Assets/02.Scripts/UI/LoadingSceneManager.cs
@@ -4,49 +4,49 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LoadingSceneManager : MonoBehaviour {
+public class LoadingSceneManager : MonoBehaviour {	
+	
+	//싱글톤 맨듬
+	// public static LoadingSceneManager instance;
+	// public static LoadingSceneManager GetInstance()
+	// {
+	// 	if(!instance)
+	// 	{
+	// 		instance=FindObjectOfType(typeof(LoadingSceneManager))as LoadingSceneManager;
+	// 		//as: 프리팹을 인스턴스화시켜준답디다
+
+	// 		if(!instance) Debug.LogError("없어");			
+	// 	}
+		
+	// 	return instance;
+	// }
+	//============================================
 
 	public Slider slider;
 	bool IsDone=false;
 	float fTime=0f;
-	AsyncOperation async_operation;	
-	public string SceneName;
-	
-	
-	//============================================
-	//싱글톤 맨듬
-	public static LoadingSceneManager instance;
-	public static LoadingSceneManager GetInstance()
-	{
-		if(!instance)
-		{
-			instance=FindObjectOfType(typeof(LoadingSceneManager))as LoadingSceneManager;
-			//as:프리팹을 인스턴스화시켜준답디다
+	AsyncOperation async_operation;		
+	//public string SceneName;
 
-			if(!instance) Debug.LogError("없어");					
-		}
-		
-		return instance;
-	}
 
 	//안터지게 함
 	void Awake()
-	{
-		if(instance==null)
-		{
-			instance=this;	
-		}
-		else if(instance!=this)
-		{
-			Destroy(gameObject);
-		}
-		DontDestroyOnLoad(gameObject);
+	{		
+		// if(instance==null)
+		// {
+		// 	instance=this;	
+		// }
+		// else if(instance!=this)
+		// {
+		// 	Destroy(gameObject);
+		// }
+		// DontDestroyOnLoad(gameObject);
 	}
 
 	//============================================
 	void Start()
 	{	
-		StartCoroutine(StartLoad(SceneName));
+		StartCoroutine(StartLoad(SceneName.instance.Name));
 	}
 
 
---
+++ Zombie Gangster/Assets/02.Scripts/UI/SceneName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneName : MonoBehaviour {
+
+	public static SceneName instance;
+	
+	public string Name;
+	
+	void Awake()
+	{
+		if(!instance)
+		{
+			instance=FindObjectOfType(typeof(SceneName))as SceneName;
+			//as:프리팹을 인스턴스화시켜준답디다
+
+			if(!instance) Debug.LogError("없어");					
+		}
+		
+		//NowSceneName.Name = NowSceneName;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	
+
+
+}

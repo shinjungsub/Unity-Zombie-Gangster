852475ca913af23558af8c5d81f5f6c851654449
shinjungsub <48083280+shinjungsub@users.noreply.github.com>
2019-05-28 22:49:41 +0900

    ## 신정섭 ## 플레이어

---
+++ Zombie Gangster/Assets/02.Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public float speed = 3f;
+    private bool attack = false;
+    int eating = 0;
+
+    Vector3 rotation = Vector3.zero;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        rotation = transform.eulerAngles;
+        animator = GetComponent<Animator>();
+        eating = Animator.StringToHash("Base Layer.Zombie_Eating");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Zombie_Eating"))
+        {
+            Debug.Log("이팅상태");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Zombie_Eating"))
+        {
+            if (coll.gameObject.tag == "Enemy")
+            {
+                Destroy(coll.gameObject);
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        animator.Play("Zombie_Eating");
+    }
+}

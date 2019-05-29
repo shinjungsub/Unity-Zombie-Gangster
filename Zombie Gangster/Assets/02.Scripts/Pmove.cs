f5f746f72e85238843ccf101ed8d8a0ab3e2fb9c
parang8146 <parang8146@gmail.com>
2019-05-29 09:34:30 +0900

    ##이병윤##건물충돌

---
+++ Zombie Gangster/Assets/02.Scripts/Pmove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pmove : MonoBehaviour
+{
+
+    public float rot_speed = 50.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(new Vector3(0, 0, 5) * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            float Key = rot_speed * Time.deltaTime;
+            transform.rotation *= Quaternion.AngleAxis(Key, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            float Key = rot_speed * Time.deltaTime;
+            transform.rotation *= Quaternion.AngleAxis(Key, -Vector3.up);
+        }
+    }
+}
